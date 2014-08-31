84083d0e8e68f6f6a10647e9be358ff7958d3352
Wieland Hoffmann <themineo@gmail.com>
2014-08-31 20:49:38 +0200

    print the playlist

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,13 @@
 import Data.Aeson.Encode.Pretty (encodePretty)
 import Edabo.MPD (getTracksFromPlaylist)
+import Edabo.Types (Playlist(Playlist))
 import qualified Data.ByteString.Lazy.Char8 as B
 
 main :: IO ()
 main = do
-     tl <- getTracksFromPlaylist
-     case tl of
-       Left e -> putStrLn e
-       Right tracks -> B.putStrLn $  encodePretty tracks
+  tl <- getTracksFromPlaylist
+  case tl of
+    Left e -> putStrLn e
+    Right tracks -> B.putStrLn
+                    $ encodePretty
+                    $ Playlist "current" Nothing tracks
