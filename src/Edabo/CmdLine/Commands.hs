621e538723051a5c17f3f045204ce629b153b5bb
Wieland Hoffmann <themineo@gmail.com>
2014-09-09 20:47:14 +0200

    clarify a docstring

--- src/Edabo/CmdLine/Commands.hs
+++ src/Edabo/CmdLine/Commands.hs
@@ -27,8 +27,8 @@ save SaveOptions {optPretty = pretty
   where encoder = if pretty then encodePretty else encode
 
 -- | The 'playlistActor' function tries to get the tracklist and, in case that
---   didn't work (a Left was returned), prints the error message or applys a
---   function to it.
+--   didn't work (a Left was returned), prints the error message or (in case a
+--   Right was returned)applys a function to it.
 playlistActor :: ([Track] -> IO ()) -- ^ A function to apply to a list of tracks
               -> IO ()
 playlistActor f = getTracksFromPlaylist >>= either putStrLn f
